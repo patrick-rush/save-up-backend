65cfcb0568222a8edfc2da5f98b5a148348e5903
patrick-rush <patrick.a.rush@gmail.com>
2021-04-04 10:38:59 -0400

    add user model

--- app/models/list.rb
+++ app/models/list.rb
@@ -1,5 +1,6 @@
 class List < ApplicationRecord
     has_many :items
+    belongs_to :user
 
     validates :type, inclusion: ["want", "wish", "wait"]
 end
--- db/migrate/20210404140705_create_lists.rb
+++ db/migrate/20210404140705_create_lists.rb
@@ -2,6 +2,7 @@ class CreateLists < ActiveRecord::Migration[6.0]
   def change
     create_table :lists do |t|
       t.string :type
+      t.references :user, null: false, foreign_key: true
 
       t.timestamps
     end
